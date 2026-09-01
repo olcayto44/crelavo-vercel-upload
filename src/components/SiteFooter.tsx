e6dfa359bc60d6665a2a60b07717229601318a12
olcayto44 <aydincak6@gmail.com>
2026-09-02 02:25:54 +0300

    Add TikTok Pixel tracking to site footer

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { HardReloadLink } from "@/components/HardReloadLink";
 
 function SocialIcon({ name }: { name: "instagram" | "youtube" | "tiktok" | "x" | "linkedin" | "reddit" }) {
@@ -12,6 +13,41 @@ function SocialIcon({ name }: { name: "instagram" | "youtube" | "tiktok" | "x" |
 export async function SiteFooter() {
   return (
     <footer className="container footer clean-feed-section site-footer">
+      <Script id="tiktok-pixel" strategy="afterInteractive">
+        {`!function (w, d, t) {
+  w.TiktokAnalyticsObject = t;
+  var ttq = w[t] = w[t] || [];
+  ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie", "holdConsent", "revokeConsent", "grantConsent"];
+  ttq.setAndDefer = function (t, e) {
+    t[e] = function () {
+      t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+    };
+  };
+  for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
+  ttq.instance = function (t) {
+    for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(e, ttq.methods[n]);
+    return e;
+  };
+  ttq.load = function (e, n) {
+    var r = "https://analytics.tiktok.com/i18n/pixel/events.js";
+    ttq._i = ttq._i || {};
+    ttq._i[e] = [];
+    ttq._i[e]._u = r;
+    ttq._t = ttq._t || {};
+    ttq._t[e] = +new Date;
+    ttq._o = ttq._o || {};
+    ttq._o[e] = n || {};
+    n = document.createElement("script");
+    n.type = "text/javascript";
+    n.async = true;
+    n.src = r + "?sdkid=" + e + "&lib=" + t;
+    e = document.getElementsByTagName("script")[0];
+    e.parentNode.insertBefore(n, e);
+  };
+  ttq.load("DABLK7JC77U21D59TE90");
+  ttq.page();
+}(window, document, "ttq");`}
+      </Script>
       <div className="site-footer-grid balanced-footer-grid">
         <div className="site-footer-group footer-core-services">
           <h3>Start here</h3>
